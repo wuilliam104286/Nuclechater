6fcae5a52f69a883d6448fd4c1b15deba6ae1422
wuilliam104286 <wuilliamolgo104286@gmail.com>
2020-07-15 14:40:42 +0800

    left

--- app.js
+++ app.js
@@ -32,13 +32,11 @@ let userlist = [];
 let iplist = [];
 let script_detect = [];
 let count = 0;
-let news = false;
 let reapet = false;
-let time;
 let asiaTime = new Date().toLocaleString("en-US", { timeZone: "Asia/Taipei" });
 let Today = new Date((new Date(asiaTime)).toISOString());
 console.log(Today)
-let date_ = Today.getFullYear() + '/' + (Today.getMonth() + 1) + '/' + Today.getDate() + '     ' + Today.getHours() + ':' + Today.getMinutes();
+let time = Today.getFullYear() + '/' + (Today.getMonth() + 1) + '/' + Today.getDate() + '     ' + Today.getHours() + ':' + Today.getMinutes();
 const random = Math.floor(Math.random() * (4294967 - 42949 + 1) + 42949);
 fs.readFile('save.json', 'utf8', function readFileCallback(err, data) {
   if (err) {
@@ -47,7 +45,7 @@ fs.readFile('save.json', 'utf8', function readFileCallback(err, data) {
     if (data) {
       obj = JSON.parse(data);
       content = obj;
-      content.push({ "name": "日期", "text": date_ });
+      content.push({ "name": "日期", "text": time });
       let json = JSON.stringify(content);
       fs.writeFile('save.json', json, 'utf8', function (err) {
         if (err)
@@ -61,65 +59,76 @@ serv_io.sockets.on('connection', function (socket) {
   socket.id = count;
   socket.on('disconnect', function () {
     let txt = iplist[socket.id];
-    console.log(userlist[txt] + "離開了聊天室");
-    //content.push({ "name": "伺服器", "text": userlist[txt] + "離開了聊天室", "time": time });
-    //socket.emit('chat', { "chat": content, "user": userlist[txt], "news": news });
-    delete iplist[socket.id];
-    delete script_detect[socket.id];
+    if (!have_ip(txt)) return;
+    data_emit("伺服器", userlist[txt] + "離開了聊天室", txt)
     let json = JSON.stringify(content);
     fs.writeFile('save.json', json, 'utf8', function (err) {
       if (err)
         console.log(err);
     });
+    iplist[socket.id] = 0;
   });
   setInterval(() => {
     socket.emit('chat', { "chat": content, "user": false, "online": serv_io.engine.clientsCount });
-    Today = new Date();
-    time = Today.getHours() + ":" + Today.getMinutes().toString().padStart(2, '0');
+    times();
   }, 500);
   // 接收來自於瀏覽器的資料
   socket.on('client_data', function (data) {
-    let realuser = false;
-    let ip = data.ip.replace(/\./g, "");
-    for (i = 0; i < iplist.length + 1; i++) if (iplist[i] === ip) realuser = true;
-    if (data.text === "" || !realuser || data.text.length > 300) return console.log("阻止了一個不法請求");
-    news = true;
+    if (data.text === "" || !have_ip(data.ip) || data.text.length > 300) {
+      if (!have_ip(data.ip)) illegal("ip undefined");
+      if (data.text.length > 300) illegal("text too long");
+      if (data.text === "") illegal("empty text");
+      return console.log("阻止了一個不法請求並要求重新連接");
+    }
     let txt = data.text;
-    if (Today.getMinutes() % 10 === 0) {
+    ip = have_ip(data.ip, true);
+    if (script_detect[ip] > 100) return illegal("bot");
+    data_emit(userlist[ip], txt, data.ip);
+    script_detect[ip]++;
+    if (Today.getMinutes() % 10 < 3) {
       for (let i = 1; i < script_detect.length; i++) script_detect[ip] = 0;
     }
-    if (script_detect[ip] > 100) return socket.emit('script', { "type": "bot" });
-    times();
-    content.push({ "name": userlist[ip], "text": txt, "time": time });
-    if (content.length > 1000) content.shift();
-    socket.emit('chat', { "chat": content, "user": false, "news": news });
-    console.log({ "name": userlist[ip], "text": txt, "ip": ip })
-    script_detect[ip]++;
-    news = false;
   });
   socket.on('ip', function (data) {
     if (!data.ip) return;
-    let txt = data.ip;
-    txt = txt.replace(/\./g, "");
-    for (i = 0; i < iplist.length; i++) if (iplist[i] === txt) reapet = true;
-    if (reapet) {
+    let txt;
+    if (isNaN(data.ip)) txt = data.ip.replace(/\./g, "");
+    if (isNaN(txt)) return;
+    if (have_ip(data.ip)) {
       socket.emit('chat', { "chat": content, "user": userlist[txt] });
-      reapet = false;
-      return;
+      return reapet = false;
     }
     iplist[socket.id] = txt;
     script_detect[socket.id] = 0;
     userlist[txt] = (parseInt(txt) + random).toString(35);
-    console.log(userlist[txt] + "加入了聊天室");
-    times();
-    content.push({ "name": "伺服器", "text": userlist[txt] + "加入了聊天室", "time": time });
-    socket.emit('chat', { "chat": content, "user": userlist[txt], "news": news });
-    if (content.length > 1000) content.shift();
+    data_emit("伺服器", userlist[txt] + "加入了聊天室", "server", userlist[txt]);
   });
+
+
+
+  function data_emit(name, text, ip, user) {
+    times();
+    if (!ip) ip = "server";
+    if (!user) user = false;
+    content.push({ "name": name, "text": text, "time": time });
+    if (content.length > 1000) content.shift();
+    socket.emit('chat', { "chat": content, "user": user, "news": true, "ip": ip });
+    console.log(name + ": " + text);
+  }
+  function times() {
+    asiaTime = new Date().toLocaleString("en-US", { timeZone: "Asia/Taipei" });
+    Today = new Date((new Date(asiaTime)).toISOString());
+    time = Today.getHours() + ":" + Today.getMinutes().toString().padStart(2, '0');
+  }
+  function have_ip(ip, get) {
+    if (!ip) return;
+    if (isNaN(ip)) ip = ip.replace(/\./g, "");
+    if (isNaN(ip)) return;
+    if (get) return ip;
+    for (i = 0; i < iplist.length + 1; i++) if (iplist[i] === ip) return true;
+  }
+  function illegal(type) {
+    socket.emit('script', { "type": type });
+    console.log("無效的操作: " + type);
+  }
 });
-function times() {
-  asiaTime = new Date().toLocaleString("en-US", { timeZone: "Asia/Taipei" });
-  Today = new Date((new Date(asiaTime)).toISOString());
-  time = Today.getHours() + ":" + Today.getMinutes().toString().padStart(2, '0');
-  //hours = Today.getHours() + ":" + Today.getMinutes().toString().padStart(2, '0');
-}
